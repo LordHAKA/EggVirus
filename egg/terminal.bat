d77e0bb2ac1503506d32d3f02b6750af8fe3e418
Lord HAKA <reception.haka@gmail.com>
2025-03-26 10:44:47 +1100

    Create terminal.bat

---
+++ egg/terminal.bat
@@ -0,0 +1,28 @@
+@echo off
+title Welcome To The Terminal Processor
+echo Starting Processor.
+echo ╔═════════════════════╗
+echo ║████                 ║
+echo ╚═════════════════════╝
+timeout 1 >nul
+cls
+echo Starting Processor..
+echo ╔═════════════════════╗
+echo ║█████████████        ║
+echo ╚═════════════════════╝
+timeout 1 >nul
+cls
+echo Starting Processor...
+echo ╔═════════════════════╗
+echo ║█████████████████████║
+echo ╚═════════════════════╝
+timeout 1 >nul
+cls
+echo ERROR ERROR ERROR ERROR ERROR
+echo ╔═════════════════════╗
+echo ║█████████████████████║
+echo ╚═════════════════════╝
+echo MALWARE IN DISK WALWARE IN DISK
+timeout 1 >nul
+cls
+start egg.bat
